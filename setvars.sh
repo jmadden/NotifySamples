53deeb4175d7160333173b934e3556f21ee026da
Stacy David <31826992+tigerfarm@users.noreply.github.com>
2018-05-21 14:33:54 -0700

    Create setvars.sh

---
+++ setvars.sh
@@ -0,0 +1,25 @@
+echo "+++ Sets variables."
+
+# ------------------------------------------------------------------------------
+# For first demo/lab:
+ACCOUNT_SID=your_account_SID
+AUTH_TOKEN=your_account_auth_token
+YOUR_PHONE_NUMBER=your_phone_number
+export ACCOUNT_SID
+export AUTH_TOKEN
+export YOUR_PHONE_NUMBER
+
+NOTIFY_SERVICE_SID=your_notify_service_sid
+export NOTIFY_SERVICE_SID
+
+# For second demo/lab:
+PHONE_NUMBER_1=your_test_phone_number_1
+PHONE_NUMBER_2=your_test_phone_number_2
+PHONE_NUMBER_3=your_test_phone_number_3
+export PHONE_NUMBER_1
+export PHONE_NUMBER_2
+export PHONE_NUMBER_3
+
+python echoVars.py
+
+# ------------------------------------------------------------------------------
