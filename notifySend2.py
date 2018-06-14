6648b1f84abccf23798774b1a527e9ca6beb2159
tigerfarm <tigerfarm@gmail.com>
2018-06-13 17:35:49 -0700

    Update

--- notifySend2.py
+++ notifySend2.py
@@ -8,7 +8,7 @@ auth_token = os.environ.get("AUTH_TOKEN")
 notify_service_sid = os.environ.get("NOTIFY_SERVICE_SID")
 client = Client(account_sid, auth_token)
 notification = client.notify.services(notify_service_sid).notifications.create(
-    body='Hello one tag.',
+    body='Hello Paul.',
     identity="paul"
 )
 print("+ Notification SID: " + notification.sid)
